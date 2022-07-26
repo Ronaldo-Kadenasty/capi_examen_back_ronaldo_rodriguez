c9681fb71d51d14bbfe056829f7cd69d6005d3b3
th3-code <operaciones.gcuarzo@gmail.com>
2022-07-25 19:02:13 -0500

    first commit

---
+++ database/migrations/2022_07_25_235603_create_user_domicilio.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilio', function (Blueprint $table) {
+            $table->id();
+            $table->text('domicilio');
+            $table->integer('numero_exterior');
+            $table->text('colonia');
+            $table->string('cp');
+            $table->text('ciudad');
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilio');
+    }
+};
